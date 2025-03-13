d48598ae44e793ed5cd7d118029a71aea62e3c26
shady-abdelhamid <shady.abdelhamid@outlook.com>
2025-03-13 17:28:20 +0200

    Add @storybook/addon-designs and storybook-dark-mode dependencies; update Storybook configuration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -13,7 +13,9 @@ const config: StorybookConfig = {
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
-    "@storybook/addon-a11y"
+    "@storybook/addon-a11y",
+    "@storybook/addon-designs",
+    "storybook-dark-mode"
   ],
   "framework": {
     "name": "@storybook/angular",
