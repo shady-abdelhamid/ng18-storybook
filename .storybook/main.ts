02119e6619f8d6d6cc21b1cd20503442595ee811
shady-abdelhamid <shady.abdelhamid@outlook.com>
2025-03-13 17:08:16 +0200

    Add @storybook/addon-a11y dependency and update Storybook configuration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -12,7 +12,8 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y"
   ],
   "framework": {
     "name": "@storybook/angular",
